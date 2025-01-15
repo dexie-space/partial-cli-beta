0286b4f2c87f53ebd2cfed1ea6d10e63173cc0da
Karlkim Suwanmongkol <karlkim@gmail.com>
2025-01-14 19:08:16 -0600

    test take partial -- wip

--- tests/puzzles/take.py
+++ tests/puzzles/take.py
@@ -55,28 +55,29 @@ ONE_32 = bytes32([1] * 32)
 
 coin_id = std_hash(ONE_32)
 
+partial_info = PartialInfo(
+    fee_puzzle_hash=FEE_PH,
+    fee_rate=FEE_RATE,
+    maker_puzzle_hash=MAKER_PH,
+    clawback_mod=Program.to(0),
+    offer_asset_id=OFFER_TAIL_HASH,
+    offer_mojos=OFFER_MOJOS,
+    request_asset_id=REQUEST_TAIL_HASH,
+    request_mojos=REQUEST_MOJOS,
+)
+
+partial_puzzle = partial_info.to_partial_puzzle()
+coin_ph = partial_puzzle.get_tree_hash()
+req_mojos_puzzle = REQ_MOJOS_MOD.curry(OFFER_MOJOS, REQUEST_MOJOS)
+
 
 class TestPartialTake:
-    partial_info = PartialInfo(
-        fee_puzzle_hash=FEE_PH,
-        fee_rate=FEE_RATE,
-        maker_puzzle_hash=MAKER_PH,
-        clawback_mod=Program.to(0),
-        offer_asset_id=OFFER_TAIL_HASH,
-        offer_mojos=OFFER_MOJOS,
-        request_asset_id=REQUEST_TAIL_HASH,
-        request_mojos=REQUEST_MOJOS,
-    )
 
-    req_mojos_puzzle = REQ_MOJOS_MOD.curry(OFFER_MOJOS, REQUEST_MOJOS)
-
-    def test_request_mojos_of_1e12(self):
+    def test_partial_take(self):
         coin_amount = uint64(2e12)
         taken_amount = uint64(0.7e12)
-        p = self.partial_info.to_partial_puzzle()
-        coin_ph = p.get_tree_hash()
         s = Program.to([coin_amount, coin_id, coin_ph, taken_amount])
-        result = p.run(s)
+        result = partial_puzzle.run(s)
         conditions = parse_conditions_non_consensus(
             result.as_iter(), abstractions=False
         )
@@ -92,9 +93,7 @@ class TestPartialTake:
 
         # # assert announcement from taker settlement
         # (ASSERT_PUZZLE_ANNOUNCEMENT  0xd2d8bc6e388f8787ab9d7996c56545ec8cb7803ea7bda04990e6d3bee92dc387)
-        expected_request_mojos = self.req_mojos_puzzle.run(
-            [uint64(taken_amount)]
-        ).as_int()
+        expected_request_mojos = req_mojos_puzzle.run([uint64(taken_amount)]).as_int()
         assert expected_request_mojos == uint64(210e3)
         maker_request_payments = Program.to(
             [
@@ -108,13 +107,13 @@ class TestPartialTake:
         )
         apa = CreatePuzzleAnnouncement(
             msg=maker_request_payments.get_tree_hash(),
-            puzzle_hash=self.partial_info.get_request_settlement_hash(),
+            puzzle_hash=partial_info.get_request_settlement_hash(),
         ).corresponding_assertion()
         assert condition_exists(conditions, apa)
 
         # create settlement coin
         # (CREATE_COIN  0xcfbfdeed5c4ca2de3d0bf520b9cb4bb7743a359bd2e6a188d19ce7dffc21d3e7 0x00a15a04d200 (0xcfbfdeed5c4ca2de3d0bf520b9cb4bb7743a359bd2e6a188d19ce7dffc21d3e7))
-        fee = FEE_MOD.run([self.partial_info.fee_rate, taken_amount]).as_int()
+        fee = FEE_MOD.run([partial_info.fee_rate, taken_amount]).as_int()
         taken_amount_after_fee = taken_amount - fee
         assert condition_exists(
             conditions,
@@ -146,4 +145,80 @@ class TestPartialTake:
                 amount=new_amount,
                 memos=[coin_ph],
             ),
+        )
+
+    def test_all_take(self):
+        coin_amount = uint64(2e12)
+        taken_amount = uint64(2e12)
+        s = Program.to([coin_amount, coin_id, coin_ph, taken_amount])
+        result = partial_puzzle.run(s)
+        conditions = parse_conditions_non_consensus(
+            result.as_iter(), abstractions=False
+        )
+
+        # (ASSERT_MY_AMOUNT  0x01d1a94a2000)
+        assert condition_exists(conditions, AssertMyAmount(coin_amount))
+
+        # (ASSERT_MY_COIN_ID  0x72cd6e8422c407fb6d098690f1130b7ded7ec2f7f5e1d30bd9d521f015363793)
+        assert condition_exists(conditions, AssertMyCoinID(coin_id))
+
+        # # (ASSERT_MY_PUZZLEHASH  0x07f64b877708fdc1b8e1f472e94a6a79797c6dac925190cca6879c4e60e80aa9)
+        assert condition_exists(conditions, AssertMyPuzzleHash(coin_ph))
+
+        # # assert announcement from taker settlement
+        # (ASSERT_PUZZLE_ANNOUNCEMENT  0xd2d8bc6e388f8787ab9d7996c56545ec8cb7803ea7bda04990e6d3bee92dc387)
+        expected_request_mojos = req_mojos_puzzle.run([uint64(taken_amount)]).as_int()
+        assert expected_request_mojos == uint64(600e3)
+        maker_request_payments = Program.to(
+            [
+                coin_id,
+                [
+                    MAKER_PH,
+                    expected_request_mojos,
+                    [MAKER_PH],
+                ],
+            ]
+        )
+        apa = CreatePuzzleAnnouncement(
+            msg=maker_request_payments.get_tree_hash(),
+            puzzle_hash=partial_info.get_request_settlement_hash(),
+        ).corresponding_assertion()
+        assert condition_exists(conditions, apa)
+
+        # create settlement coin
+        # (CREATE_COIN  0xcfbfdeed5c4ca2de3d0bf520b9cb4bb7743a359bd2e6a188d19ce7dffc21d3e7 0x00a15a04d200 (0xcfbfdeed5c4ca2de3d0bf520b9cb4bb7743a359bd2e6a188d19ce7dffc21d3e7))
+        fee = FEE_MOD.run([partial_info.fee_rate, taken_amount]).as_int()
+        taken_amount_after_fee = taken_amount - fee
+        assert condition_exists(
+            conditions,
+            CreateCoin(
+                puzzle_hash=OFFER_MOD_HASH,
+                amount=taken_amount_after_fee,
+                memos=[OFFER_MOD_HASH],
+            ),
+        )
+
+        # create fee coin
+        # (CREATE_COIN  0xbc1f6e34fbc556d279b2e2187f2b7271df540926f21f74e26ba212c78a56b9b9 0x01a13b8600 (0xbc1f6e34fbc556d279b2e2187f2b7271df540926f21f74e26ba212c78a56b9b9))
+        assert condition_exists(
+            conditions,
+            CreateCoin(
+                puzzle_hash=FEE_PH,
+                amount=fee,
+                memos=[FEE_PH],
+            ),
+        )
+
+        # create new partial coin should not exist
+        new_amount = coin_amount - taken_amount
+        assert (
+            condition_exists(
+                conditions,
+                CreateCoin(
+                    puzzle_hash=coin_ph,
+                    amount=new_amount,
+                    memos=[coin_ph],
+                ),
+            )
+            is False
         )
